bdda2f387b88db4b128be5c42bf626f1d88b29da
Vm-ashitha-3 <150150652+Vm-ashitha-3@users.noreply.github.com>
2023-11-17 12:32:40 +0530

    Create fsgs.c

---
+++ fsgs.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,n3;
+printf("enter three numbers:");
+scanf("%d&%d&d",&n1,&n2,&n3)
+if(n1>n2)
+{
+ if(n1>n3)
+{
+printf("%d is largest",n1);
+}
+else
+{
+printf("%d is largest",n3);
+}
+}
+else
+{
+if(n2>n3)
+{
+printf("%d is largest",n2);
+}
+else
+{
+printf("%d is largest",n3);
+}
+}
+}
